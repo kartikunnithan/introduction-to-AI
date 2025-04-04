40683b7ecdad845eb84b4a4b9feb469f889b0bf1
kartikunnithan <kartikunnithan738@gmail.com>
2025-04-04 18:24:59 +0400

    9

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ elif mood=="bad":
     print("That is sad ", name)
 else:
     print("Oh its okay sometimes its hard to describe hoe you feel")
+print("So what is your favourite food?")
+food=input()
+print("Nice choice", food, "Is really tasty")
+print("What are your hobbies?")
+hobbies=input()
+print("That is great")
 print("It was nice chatting with you ", name, ".Goodbye")
